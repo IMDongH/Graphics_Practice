9e4284e50bc5606cad476e16d7418979f4f012c2
임동혁 <idh1007@naver.com>
2022-09-07 14:57:50 +0900

    .

--- 2주차코드예시/Graphics02-WebGL/triangle.js
+++ 2주차코드예시/Graphics02-WebGL/triangle.js
@@ -2,44 +2,43 @@
 var gl;
 var points;
 
-window.onload = function init()
-{
-    var canvas = document.getElementById( "gl-canvas" );
+//페이지가 로드되었을떄 불러지는 콜백 함수
+window.onload = function init() {
+    var canvas = document.getElementById("gl-canvas");
 
-    gl = WebGLUtils.setupWebGL( canvas );
-    if ( !gl ) { alert( "WebGL isn't available" ); }
+    gl = WebGLUtils.setupWebGL(canvas);
+    if (!gl) { alert("WebGL isn't available"); }
 
 
     //var vertices = new Float32Array([vec2(-1, -1), vec2(0, 1), vec2(1, -1)]);
-	  // var vertices = [ vec2(-1,-1), vec2(0,1), vec2(1,-1)];
+    // var vertices = [ vec2(-1,-1), vec2(0,1), vec2(1,-1)];
     //  Configure WebGL
-    var vertices = new Float32Array([-1, -1, 0, 1, 1, -1]);
+    var vertices = new Float32Array([-1, -0.5, -0.5, 0.5, 0, -0.5,  0, -0.5, 0.5, 0.5, 1, -0.5]);
 
-    gl.viewport( 0, 0, canvas.width, canvas.height );
-    gl.clearColor( 0.0, 0.0, 0.0, 1.0 );
+    gl.viewport(0, 0, canvas.width/2, canvas.height/2);
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
     //  Load shaders and initialize attribute buffers
-
-    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
-    gl.useProgram( program );
+    //실제 프로그램을 만들어주는 것 파라미터가 3개임 
+    var program = initShaders(gl, "vertex-shader", "fragment-shader");
+    gl.useProgram(program);
 
     // Load the data into the GPU
-
     var bufferId = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
-    gl.bufferData( gl.ARRAY_BUFFER,vertices, gl.STATIC_DRAW );
+    gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
 
     // Associate vertex data buffer with shader variables
 
-    var vPosition = gl.getAttribLocation( program, "vPosition" );
-    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( vPosition );
+    var vPosition = gl.getAttribLocation(program, "vPosition");
+    gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPosition);
 
     render();
 };
 
 
 function render() {
-    gl.clear( gl.COLOR_BUFFER_BIT );
-    gl.drawArrays( gl.TRIANGLES, 0, 3 );
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.drawArrays(gl.TRIANGLES, 0, 6);
 }
